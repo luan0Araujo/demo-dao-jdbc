3921ccec8ff532a28cd81269c49defa6c0f1e59d
Luan Araujo <luanaraujo59@gmail.com>
2020-11-05 13:24:20 -0300

    Classe Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,20 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
-	public static void main(String[] args) {
-	
+	public static void main(String[] args) throws ParseException {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		Department obj = new Department(1, "RH");
-		System.out.println(obj);
+		Date x = sdf.parse("17/12/1999");
+		Seller seller = new Seller(1, "luan", "luan@gmail.com", x, 2000.0, obj);
+		System.out.println(seller);
 	}
 
 }
